50a51204288a195f76cff002e7207dad59cb5a4a
Nipaaaa1 <Frostone224@gmail.com>
2024-04-23 16:00:05 +0700

    chore: add tailwindcss prettier plugin

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,20 @@
 
 <template>
   <div
-    class="bg-primary-pale-blue w-full h-svh p-6 grid place-items-center font-red-hat-display"
+    class="grid h-svh w-full place-items-center bg-primary-pale-blue p-6 font-red-hat-display"
   >
     <img
-      class="inset-0 absolute"
+      class="absolute inset-0"
       src="/images/pattern-background-mobile.svg"
       alt=""
     />
-    <div class="flex flex-col rounded-2xl w-full overflow-clip z-10">
+    <div class="z-10 flex w-full flex-col overflow-clip rounded-2xl">
       <img
         src="/images/illustration-hero.svg"
         alt="A person listening music illustration"
       />
       <div class="bg-neutral-very-pale-blue p-6 text-center">
-        <h1 class="font-black text-neutral-dark-blue text-xl">Order Summary</h1>
+        <h1 class="text-xl font-black text-neutral-dark-blue">Order Summary</h1>
       </div>
     </div>
   </div>
