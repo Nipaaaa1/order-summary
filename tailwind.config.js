aa40cf25e949bba9852d30448dc07679ef45e7d3
Nipaaaa1 <Frostone224@gmail.com>
2024-04-23 15:46:23 +0700

    styles: setup colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          "pale-blue": "hsl(225, 100%, 94%)",
+          "bright-blue": "hsl(245, 75%, 52%)",
+        },
+        neutral: {
+          "very-pale-blue": "hsl(225, 100%, 98%)",
+          "desaturated-blue": "hsl(224, 23%, 55%)",
+          "dark-blue": "hsl(223, 47%, 23%)",
+        },
+      },
+      fontFamily: {
+        "red-hat-display": ["Red Hat Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
