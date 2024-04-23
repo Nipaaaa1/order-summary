9b75b2126331559752b4c09c895d7280fbcf89c0
Nipaaaa1 <Frostone224@gmail.com>
2024-04-23 15:55:28 +0700

    chore: move images folder to public

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div
+    class="bg-primary-pale-blue w-full h-svh p-6 grid place-items-center font-red-hat-display"
+  >
+    <img
+      class="inset-0 absolute"
+      src="/images/pattern-background-mobile.svg"
+      alt=""
+    />
+    <div class="flex flex-col rounded-2xl w-full overflow-clip z-10">
+      <img
+        src="/images/illustration-hero.svg"
+        alt="A person listening music illustration"
+      />
+      <div class="bg-neutral-very-pale-blue p-6 text-center">
+        <h1 class="font-black text-neutral-dark-blue text-xl">Order Summary</h1>
+      </div>
+    </div>
   </div>
 </template>
 
